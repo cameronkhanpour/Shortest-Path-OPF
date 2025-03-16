6509119d9dd5a909d044f2ee9d32502e246ab8a6
cameronkhanpour <99142483+cameronkhanpour@users.noreply.github.com>
2025-03-15 22:15:46 -0400

    have fun sam :)
    nonconvex_sampling updated

--- convexity_metric.jl
+++ convexity_metric.jl
@@ -2,64 +2,14 @@ module ConvexityMetric
 
 using Statistics, Random, Printf, LinearAlgebra
 
-# Include necessary repository files.
 include("barrier_case_time_v2.jl")
 include("power_flow_rect_qc.jl")
 include("shortest_path_v2.jl")
 
-# Import helper functions from the repository modules.
 import .PowerFlowRectQC: compute_qc
 import .ShortestPathOPF: load_case, get_endpoints, run_case
 
 export sample_nonconvex_space, compute_ratio, compute_convexity_metric
-
-
-function sample_nonconvex_space(case_file::String, num_samples::Int; ind_u::Vector{Int}=nothing)
-    if case_file == "MATPOWER/case9dongchan.m" && ind_u === nothing
-        ind_u = 2:3
-    end
-
-    case_qc = compute_qc(case_file, false)
-    case_data = load_case(case_file, case_qc, ind_u)
-    
-    tol_inner = 1e-6
-
-    u_start_full, x_start, u_end_full, x_end = get_endpoints(case_data.mpc, case_data, tol_inner)
-    initial_sample = u_end_full[ind_u]
-    samples = [initial_sample]
-    @printf("Initial sample: %s\n", initial_sample)
-
-    base_u = case_data.qc_data.u0[ind_u]
-    
-    for i in 2:num_samples
-        repulsion = zeros(length(base_u))
-        for p in samples
-            diff = base_u .- p
-            d = norm(diff)
-            d = d < 1e-3 ? 1e-3 : d
-            repulsion .+= diff ./ d
-        end
-        repulsion .*= 0.1 
-
-        new_u_start = base_u .+ repulsion .+ 0.05 * randn(length(base_u))
-        new_u_end   = base_u .- repulsion .+ 0.05 * randn(length(base_u))
-        @printf("Iteration %d: Proposed endpoints:\n  new_u_start = %s\n  new_u_end   = %s\n", i, new_u_start, new_u_end)
-        
-        new_u_start_full = copy(case_data.qc_data.u0)
-        new_u_end_full   = copy(case_data.qc_data.u0)
-        new_u_start_full[ind_u] = new_u_start
-        new_u_end_full[ind_u]   = new_u_end
-
-        _ = run_case(case_file; u_start=new_u_start_full, u_end=new_u_end_full)
-        
-        u_start_full_new, x_start_new, u_end_full_new, x_end_new = get_endpoints(case_data.mpc, case_data, tol_inner, rng)
-        new_sample = u_end_full_new[ind_u]
-        @printf("Iteration %d: New sample: %s\n", i, new_sample)
-        push!(samples, new_sample)
-    end
-
-    return samples
-end
 
 function compute_ratio(u_start::Vector{Float64}, u_end::Vector{Float64}, case_file::String)
     max_violation, max_violation_after, path_diff_pc, fobj_diff_pc, total_time =
@@ -103,7 +53,7 @@ data = "MATPOWER/case9dongchan.m"
 include("nonconvex_sampling.jl")
 using .NonconvexSampling: sample_nonconvex_space
 
-sample_points = sample_nonconvex_space(data, 5; ind_u = 2:3)
+sample_points = sample_nonconvex_space(data, 5; ind_u = collect(2:3))
 @printf("Sampled control points:\n")
 for s in sample_points
     @printf("  %s\n", s)
--- nonconvex_sampling.jl
+++ nonconvex_sampling.jl
@@ -4,43 +4,23 @@ using JuMP
 using Ipopt
 using SparseArrays
 using LinearAlgebra
+import PowerModelsAnnex as PMA
+
 import MathOptInterface as MOI
 
 include("power_flow_rect_qc.jl")
-using .PowerFlowRectQC: parse_case   
+using .PowerFlowRectQC: parse_case, load_case   
 
-
-function sample_nonconvex_space(case_file::String, num_samples::Int; ind_u::Vector{Int}=nothing)
+function sample_nonconvex_space(case_file::String)
     barrier_points = Vector{Vector{Float64}}()
     solutions = Vector{Vector{Float64}}()
-    
-    for i in 1:num_samples
-        println("Sampling iteration $i ...")
-        model, qc_data = build_opf_model(case_file, barrier_points, ind_u)
-        optimize!(model)
-        status = termination_status(model)
-        if status != MOI.OPTIMAL && status != MOI.LOCALLY_SOLVED
-            println("Solver did not converge at iteration $i, status: $status")
-            break
-        end
-        sol = [value(model[:u][j]) for j in 1:length(model[:u])]
-        push!(solutions, sol)
-        push!(barrier_points, sol)
-        println("Iteration $i: Obtained a new sample; barrier updated.")
-    end
-
-    return solutions
+    mpc = load_case(case_file)
+    model = PMA.build_ac_opf(mpc)
+    return model
 end
-
+end
 
 # test
+using .NonconvexSampling: sample_nonconvex_space
 case_file = "MATPOWER/case9dongchan.m"
-
-num_samples = 5
-sols = sample_nonconvex_space(case_file, num_samples; ind_u = collect(2:3))
-println("Collected $(length(sols)) solution samples.")
-for (i, s) in enumerate(sols)
-    println("Sample $i: ", s)
-end
-
-end
+println(sample_nonconvex_space(case_file))

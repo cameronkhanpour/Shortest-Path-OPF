c7c7c97e1edd5e9a56e498e02d7be1a5b155fdf6
cameronkhanpour <99142483+cameronkhanpour@users.noreply.github.com>
2025-03-20 18:56:12 -0400

    Update nonconvex_sampling.jl

--- nonconvex_sampling.jl
+++ nonconvex_sampling.jl
@@ -11,16 +11,30 @@ import MathOptInterface as MOI
 include("power_flow_rect_qc.jl")
 using .PowerFlowRectQC: parse_case, load_case   
 
-function sample_nonconvex_space(case_file::String)
+# creates AC OPF JuMP model to give us the feasible space to sample from that particular case file 
+function nonconvex_feasible_space(case_file::String)
     barrier_points = Vector{Vector{Float64}}()
     solutions = Vector{Vector{Float64}}()
     mpc = load_case(case_file)
     model = PMA.build_ac_opf(mpc)
-    return model
-end
+
+    # gets all binding constraints of JuMP model
+    binding_cons = ConstraintRef[]
+    for (F, S) in list_of_constraint_types(model)  
+        for con in all_constraints(model, F, S)
+            if norm(dual(con)) > threshold || continue
+                push!(binding_cons, con)
+            end
+        end
+    end
+
 end
 
-# test
-using .NonconvexSampling: sample_nonconvex_space
-case_file = "MATPOWER/case9dongchan.m"
-println(sample_nonconvex_space(case_file))
+
+# hit and run algorithm to generate uniform samples from AC OPF feasible space
+function hit_and_run(is_inside::Bool, x_0::Float64, n_steps::Int64 = 10)
+
+end
+
+
+end
